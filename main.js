34842ba050d7ec5f18462157b591de6dddae89df
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-06 14:21:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,17 @@ const addTwo = newSission('Olena ');
 
 console.log(addOne(20));
 console.log(addTwo(30));
+
+
+
+function urlGeneretion (domain) {
+    return function(url) {
+        return `http://${url}.${domain}`
+    };
+};
+
+const conUrl = urlGeneretion(`com`)
+const conTwoUrl = urlGeneretion(`ua`)
+
+console.log(conUrl(`google`))
+console.log(conTwoUrl(`overtuuls`))
